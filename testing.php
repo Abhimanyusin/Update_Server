84b5cce1e70231769eca47eb3e9d868f4e86a58c
Abhimanyusin <110843280+Abhimanyusin@users.noreply.github.com>
2022-08-13 13:30:04 +0530

    Create testing.php

---
+++ testing.php
@@ -0,0 +1,10 @@
+{
+  "Visibility": "VISIBLE",
+  "Title": "Testing Update",
+  "Description": "Testing Only",
+  "MainTextButton": "Update",
+  "Cancelable": "true",
+  "CancelButtonText": "Cancel",
+  "DownloadLink": "https://sharechat.onelink.me/3i9Y/esokcmx8",
+  "Version": "1.1"
+}
